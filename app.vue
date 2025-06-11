4324199981fa8ee538ea765e666b921d7ec2dbd0
Hetari <hetari4py@gmail.com>
2025-06-11 17:05:58 +0300

    chore: update package.json to add linting scripts and new dependencies; create main.css for Tailwind CSS integration

--- app.vue
+++ app.vue
@@ -1,6 +1,3 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-7xl text-red-600 font-bold underline">Hello world!</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  // future: {
+  //   compatibilityVersion: 4
+  // },
+  app: {
+    head: {
+      title: 'Glide.ai',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        // { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  css: ['./assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
